6eeee91558bff6384ae0fda474fa797349aa4002
Mouna Tebourski <mouna.tebourski.pro@gmail.com>
2024-04-01 17:04:11 -0400

    refactor : remove nextintl configuration

--- next.config.js
+++ next.config.js
@@ -1,22 +1,13 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "images.pexels.com",
+			},
+		],
+	},
+};
 
-const createNextIntlPlugin = require("next-intl/plugin");
-
-try {
-	const withNextIntl = createNextIntlPlugin();
-
-	const nextConfig = {
-		images: {
-			remotePatterns: [
-				{
-					protocol: "https",
-					hostname: "images.pexels.com",
-				},
-			],
-		},
-	};
-
-	module.exports = withNextIntl(nextConfig);
-} catch (error) {
-	throw new Error(error);
-}
+export default nextConfig;
